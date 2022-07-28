a7ce847e45f72a1b0ce89c1488a14315eff596f5
Pedro5722 <pedroalbuquerquearaujo@gmail.com>
2022-07-28 15:06:37 -0300

    only show output when a valid CEP is typed

--- src/index.js
+++ src/index.js
@@ -3,32 +3,35 @@ let infos = {};
 //rodar a API para gerar os dados buscados
 async function api(){
   const cepSelected = document.querySelector("input").value;
-  const endpoint = `https://viacep.com.br/ws/${cepSelected}/json/`
-  const response = await fetch(endpoint).then(blob => blob.json()).then(data => infos = data )
-  console.log(response)
+  const endpoint = `https://viacep.com.br/ws/${cepSelected}/json/`;
   const result = document.querySelector('.result');
-  result.innerHTML = buscaCep();
+
+  if(cepSelected.length !== 8) return result.innerHTML = buscaCep(cepSelected)
+
+  const response = await fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => infos = data );
+
+  console.log(response)
+  result.innerHTML = buscaCep(cepSelected);
+  
 }
 
 //modifica o HTML com os dados gerados na consulta da API
 function buscaCep(cepSelected){
-
-  if(cepSelected !== ''){
+  console.log(cepSelected)
+  if(cepSelected.length === 8 && infos.erro !== "true"){
     return `
-    <li class="address">Endereço: ${infos.logradouro} ${infos.complemento}</li>
-    <li class="district">Bairro: ${infos.bairro}</li>
-    <li class="city">Cidade: ${infos.localidade}</li>
-    <li class="cep">CEP: ${infos.cep}</li>
-    `;
+    <ul>
+      <li class="cep">CEP: ${infos.cep}</li>
+      <li class="address">Endereço: ${infos.logradouro} ${infos.complemento}</li>
+      <li class="district">Bairro: ${infos.bairro}</li>
+      <li class="city">Cidade: ${infos.localidade}</li>
+    </ul>`;
   }else{
-    return `
-    <li class="address">Endereço:</li>
-    <li class="district">Bairro:</li>
-    <li class="city">Cidade:</li>
-    <li class="cep">CEP:</li>
-    `;
+    return '<ul></ul>';
   }
 }
-const cepTyped = document.querySelector("input").value;
+
 const buscar = document.getElementById("buscar");
 buscar.addEventListener("click",api);
